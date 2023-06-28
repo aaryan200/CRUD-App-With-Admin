078861aa2e297a5a3d317ac473d3a5cbe1389cec
aaryan200 <co21btech11001@iith.ac.in>
2023-06-28 19:02:25 +0530

    Changed icons on admin page

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,17 +1,19 @@
 import Box from '../Box/Box';
 import Heading from '../Heading/Heading'
 import './dashboard.css'
-import {BiTachometer} from 'react-icons/bi';
-import { AiOutlineLike, AiOutlineClockCircle } from 'react-icons/ai';
-import { FaRegComments } from 'react-icons/fa';
-import { PiShareFatBold } from 'react-icons/pi';
+import { BiTachometer } from 'react-icons/bi';
+import { AiOutlineClockCircle } from 'react-icons/ai';
+// import { AiOutlineLike, AiOutlineClockCircle } from 'react-icons/ai';
+// import { FaRegComments } from 'react-icons/fa';
+import { TiDocument } from 'react-icons/ti';
+import { PiShareFatBold, PiUsersBold } from 'react-icons/pi';
 import DataTable from '../DataTable/DataTable';
 import { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { Context } from '../../context/Context';
 
 export default function Dashboard() {
-    const {user} = useContext(Context);
+    const { user } = useContext(Context);
     const [blogs, setBlogs] = useState([]);
     const [numUsers, setNumUsers] = useState(10120);
     useEffect(() => {
@@ -28,7 +30,7 @@ export default function Dashboard() {
             try {
                 const res = await axios.get(`${process.env.REACT_APP_SERVER_URL}/users/get/count/?email=${user?.email}`);
                 setNumUsers(res.data);
-            } catch(err) {
+            } catch (err) {
                 console.log(err);
             }
         }
@@ -36,14 +38,14 @@ export default function Dashboard() {
     }, []);
     return (
         <div className={`dashboard-wrapper wrapper`}>
-            <Heading icon={<BiTachometer/>} headingText="Dashboard"/>
+            <Heading icon={<BiTachometer />} headingText="Dashboard" />
             <div className="boxes">
-                <Box icon={<AiOutlineLike/>} boxText="Total Blogs" boxNumber={blogs.length} bgColor={'var(--box1-color)'}/>
-                <Box icon={<FaRegComments/>} boxText="Users" boxNumber={numUsers} bgColor={'var(--box2-color)'}/>
-                <Box icon={<PiShareFatBold/>} boxText="Total Share" boxNumber="10120" bgColor={'var(--box3-color)'}/>
+                <Box icon={<TiDocument />} boxText="Total Blogs" boxNumber={blogs.length} bgColor={'var(--box1-color)'} />
+                <Box icon={<PiUsersBold />} boxText="Users" boxNumber={numUsers} bgColor={'var(--box2-color)'} />
+                <Box icon={<PiShareFatBold />} boxText="Total Share" boxNumber="10120" bgColor={'var(--box3-color)'} />
             </div>
-            <Heading icon={<AiOutlineClockCircle/>} headingText="Recent Activity"/>
-            <DataTable blogs={blogs}/>
+            <Heading icon={<AiOutlineClockCircle />} headingText="Recent Activity" />
+            <DataTable blogs={blogs} />
         </div>
     )
 }
